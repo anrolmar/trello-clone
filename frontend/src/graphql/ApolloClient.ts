9e58cc60e9a0534ebb47f31848b50447848335a7
Angel Roldan <Angel.Roldan@nestle.com>
2022-10-04 22:09:35 +0200

    User Authentication and Data Scoping

--- frontend/src/graphql/ApolloClient.ts
+++ frontend/src/graphql/ApolloClient.ts
@@ -4,10 +4,38 @@ import {
   createHttpLink,
 } from "@apollo/client/core";
 
+import { onError } from "@apollo/client/link/error";
+import { setContext } from "@apollo/client/link/context";
+import { useAuthUserStore } from "@/stores";
+
 // HTTP connection to the API
 const httpLink = createHttpLink({
   // You should use an absolute URL here
   uri: "https://api.8base.com/cl6dfqj9k00a409mhf8c29pjr",
+});
+
+const setAuthorizationLink = setContext((request, previousContext) => {
+  const store = useAuthUserStore();
+  return store.authenticated
+    ? {
+        ...previousContext,
+        headers: {
+          authorization: `Bearer ${store.tokenId}`,
+        },
+      }
+    : previousContext;
+});
+
+// Error handling
+const setErrorHandler = onError((error) => {
+  const badToken = !!error.response?.errors?.find(
+    (e: { code: string }) =>
+      e.code === "TokenExpiredError" || e.code === "InvalidTokenError"
+  );
+  if (badToken) {
+    const store = useAuthUserStore();
+    store.login();
+  }
 });
 
 // Cache implementation
@@ -15,7 +43,7 @@ const cache = new InMemoryCache();
 
 // Create the apollo client
 const apolloClient = new ApolloClient({
-  link: httpLink,
+  link: setAuthorizationLink.concat(setErrorHandler).concat(httpLink),
   cache,
 });
 
--- frontend/src/stores/authUser.ts
+++ frontend/src/stores/authUser.ts
@@ -1,4 +1,7 @@
+import GET_CURRENT_USER from "@/graphql/queries/auth/currentUser.query.gql";
+import USER_SIGN_UP from "@/graphql/mutations/users/userSignUp.mutation.gql";
 import type { User } from "@/types";
+import apolloClient from "@/graphql/ApolloClient";
 import { authClient } from "@/authentication/8baseAuth";
 import { defineStore } from "pinia";
 
@@ -22,10 +25,68 @@ const useAuthUserStore = defineStore("authUser", {
       authClient.authorize();
     },
     logout() {
-      authClient.signOut();
+      authClient.logout();
+      this.authenticated = false;
+      this.tokenId = null;
+      localStorage.removeItem(localStorageKey);
+    },
+    async initUser() {
+      if (!this.tokenId) return;
+      try {
+        const {
+          data: { user },
+        } = await fetchUser(this.tokenId);
+        this.user = user;
+      } catch (error) {
+        console.error("No existing user matching with the token id");
+      }
+    },
+    async handleAuthentication() {
+      const { idToken, email, firstName } =
+        await authClient.getAuthorizedData();
+      try {
+        await fetchUser(idToken);
+      } catch {
+        await apolloClient.mutate({
+          mutation: USER_SIGN_UP,
+          variables: {
+            user: {
+              email,
+              team: {
+                create: {
+                  name: `${firstName}'s team`,
+                },
+              },
+            },
+            authProfileId: import.meta.env.VITE_AUTH_PROFILE_ID,
+          },
+          context: {
+            headers: {
+              authorization: `Bearer ${idToken}`,
+            },
+          },
+        });
+      }
+
+      this.authenticated = true;
+      this.tokenId = idToken;
+      localStorage.setItem(localStorageKey, idToken);
     },
   },
   getters: {},
 });
+
+const fetchUser = async (tokenId: string) => {
+  const context = {
+    headers: {
+      authorization: `Bearer ${tokenId}`,
+    },
+  };
+
+  return apolloClient.query({
+    query: GET_CURRENT_USER,
+    context,
+  });
+};
 
 export default useAuthUserStore;
